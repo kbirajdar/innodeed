6f390cb30eee90099ab44a7ed59d446777734b79
kbirajdar <kbirajdar352@gmail.com>
2022-11-11 15:14:06 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import url from './public/api/cars.json'
 import Cars from './Cars';
-import Items from './Items';
-
-
-const breakPoints = [
-  { width: 1, itemsToShow: 1 },
-  { width: 550, itemsToShow: 2, itemsToScroll: 2 },
-  { width: 768, itemsToShow: 3 },
-  { width: 1200, itemsToShow: 4 }
-];
-
 
 function App() {
   const[cars,setCars]=useState([]);
@@ -22,13 +12,11 @@ function App() {
   },[]);
   return (
     <div className="App">
-      <div className="carousel-wrapper">
-        <Carousel breakPoints={breakPoints}>
-          {cars.map((item) => (
-            <Items><Cars ditails={item}/></Items>
-          ))}
-        </Carousel>
-      </div>
+      
+      {cars.map((item) => (
+       <Cars ditails={item}/>
+      ))}
+        
     </div>
   );
 }
