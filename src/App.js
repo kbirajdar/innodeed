418037fa92cb26db2feef9f314bac611c7dd3507
kbirajdar <kbirajdar352@gmail.com>
2022-11-11 14:19:20 +0530

    first

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import url from './public/api/cars.json'
 import Cars from './Cars';
+import Items from './Items';
+
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 4 }
+];
 
 
 function App() {
@@ -13,7 +22,13 @@ function App() {
   },[]);
   return (
     <div className="App">
-      {cars.map((items)=><Cars ditails={items}/>)}
+      <div className="carousel-wrapper">
+        <Carousel breakPoints={breakPoints}>
+          {cars.map((item) => (
+            <Items><Cars ditails={item}/></Items>
+          ))}
+        </Carousel>
+      </div>
     </div>
   );
 }
